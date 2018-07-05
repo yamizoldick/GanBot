07e54f49aab99921423c0e8bedd6d3cf9ecc57ed
yamizoldick <40841290+yamizoldick@users.noreply.github.com>
2018-07-05 09:42:36 -0500

    Basedatossql
    Se agrega el script SQL

---
+++ cinetop.sql
@@ -0,0 +1,224 @@
+﻿use cinetop;
+
+--
+-- Base de datos: `cinetop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id_cliente` int(5) NOT NULL,
+  `tipo_cliente` int(5) NOT NULL,
+  `nom_cliente` int(50) NOT NULL,
+  `pago_cliente` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `combo`
+--
+
+CREATE TABLE `combo` (
+  `id_combo` int(2) NOT NULL,
+  `nom_combo` varchar(20) NOT NULL,
+  `precio_combo` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `contenido`
+--
+
+CREATE TABLE `contenido` (
+  `id_combo` int(3) NOT NULL,
+  `id_articulo` int(2) NOT NULL,
+  `cantidad` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `despedida`
+--
+
+CREATE TABLE `despedida` (
+  `id_despe` int(2) NOT NULL,
+  `resp_despe` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `dulceria`
+--
+
+CREATE TABLE `dulceria` (
+  `id_articulo` int(3) NOT NULL,
+  `nom_articulo` int(30) NOT NULL,
+  `tipo_articulo` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `funcion`
+--
+
+CREATE TABLE `funcion` (
+  `id_peli` int(5) NOT NULL,
+  `id_sala` int(2) NOT NULL,
+  `hora_func` time NOT NULL,
+  `precio_func` int(3) NOT NULL,
+  `idioma` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pelicula`
+--
+
+CREATE TABLE `pelicula` (
+  `id_peli` int(5) NOT NULL,
+  `nom_peli` varchar(30) NOT NULL,
+  `clas_peli` varchar(1) NOT NULL,
+  `des_peli` varchar(255) NOT NULL,
+  `hora_peli` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sala`
+--
+
+CREATE TABLE `sala` (
+  `id_sala` int(2) NOT NULL,
+  `tipo_sala` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `saludo`
+--
+
+CREATE TABLE `saludo` (
+  `id_saludo` int(2) NOT NULL,
+  `resp_saludo` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id_cliente`);
+
+--
+-- Indices de la tabla `combo`
+--
+ALTER TABLE `combo`
+  ADD PRIMARY KEY (`id_combo`);
+
+--
+-- Indices de la tabla `contenido`
+--
+ALTER TABLE `contenido`
+  ADD PRIMARY KEY (`id_combo`);
+
+--
+-- Indices de la tabla `despedida`
+--
+ALTER TABLE `despedida`
+  ADD PRIMARY KEY (`id_despe`);
+
+--
+-- Indices de la tabla `dulceria`
+--
+ALTER TABLE `dulceria`
+  ADD PRIMARY KEY (`id_articulo`);
+
+--
+-- Indices de la tabla `pelicula`
+--
+ALTER TABLE `pelicula`
+  ADD PRIMARY KEY (`id_peli`);
+
+--
+-- Indices de la tabla `sala`
+--
+ALTER TABLE `sala`
+  ADD PRIMARY KEY (`id_sala`);
+
+--
+-- Indices de la tabla `saludo`
+--
+ALTER TABLE `saludo`
+  ADD PRIMARY KEY (`id_saludo`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id_cliente` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `combo`
+--
+ALTER TABLE `combo`
+  MODIFY `id_combo` int(2) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `contenido`
+--
+ALTER TABLE `contenido`
+  MODIFY `id_combo` int(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `despedida`
+--
+ALTER TABLE `despedida`
+  MODIFY `id_despe` int(2) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `dulceria`
+--
+ALTER TABLE `dulceria`
+  MODIFY `id_articulo` int(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `pelicula`
+--
+ALTER TABLE `pelicula`
+  MODIFY `id_peli` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `sala`
+--
+ALTER TABLE `sala`
+  MODIFY `id_sala` int(2) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `saludo`
+--
+ALTER TABLE `saludo`
+  MODIFY `id_saludo` int(2) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
